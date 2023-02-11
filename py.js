2dcc239ff4d2a097a9a4aacf84f56ccfe9b75e36
eduu17 <sranjel74@gmail.com>
2023-02-11 17:45:50 -0300

    First commit

---
+++ py.js
@@ -0,0 +1,40 @@
+// funcion nomefunction(parametro(Guarda valor));
+// nomefunction(Valor a ser guardado);
+
+function getNoname() {
+  console.log("Função sem valor");
+}
+
+//getNoname();
+
+function pacoca(dos) {
+  console.log(dos);
+}
+
+//pacoca("Lata de paçoca");
+
+function getSoma(n1, n2) {
+  console.log(n1 + n2);
+}
+
+//getSoma(2, 2);
+
+function getDados(name, idade, filme) {
+  console.log(
+    `Meu nome é ${name}, minha idade é ${idade} e meu filme favorito é ${filme}`
+  );
+}
+//getDados("Osvaldo", 51, "Spider Man Noir");
+
+function mat(n1) {
+  return n1 * 3;
+}
+
+//console.log(mat(4));
+
+
+
+
+
+
+
